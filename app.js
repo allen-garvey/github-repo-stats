f73139fea1829f3da358881fbe984a047c0c0b20
Allen Garvey <allen@allengarvey.com>
2016-02-22 19:15:30 -0500

    add markup and classes for styling language text and use percentage for bar graph width

--- app.js
+++ app.js
@@ -10,9 +10,9 @@
     	var language_collection = languages_set.getSortedCollection();
     	var $languages_list = $('#languages_list');
     	$.each(language_collection, function(index, val) {
-    		 var text = val.name + ' ' + val.amount;
+    		 var text = val.name + ' - ' + val.amount;
     		 console.log(text);
-    		 $languages_list.append('<li>' + text + '</li>');
+    		 $languages_list.append('<li><span class="list_text">' + text + '</span><div class="bar" style="width:' +  val.percentage_amount + '%"></div></li>');
     	});
     });
 })(jQuery);
