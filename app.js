a33777a5024c6246b8557d0f578bd31f3e786c65
Allen Garvey <allengarvey@hotmail.com>
2016-02-23 19:49:31 -0500

    add google pie chart of repos by language

--- app.js
+++ app.js
@@ -2,17 +2,53 @@
  * 
  */
 (function($){
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {'packages':['corechart']});
 	var languages_set = new WDP.countedSet();
+    var language_list = [];
     $.get('https://api.github.com/users/allen-garvey/repos?per_page=100', function(repos) {
     	$.each(repos, function(index, repo) {
     		 languages_set.add(repo.language);
     	});
-    	var language_collection = languages_set.getSortedCollection();
-    	var $languages_list = $('#languages_list');
-    	$.each(language_collection, function(index, val) {
+    	language_list = languages_set.getSortedCollection();
+        // Set a callback to run when the Google Visualization API is loaded.
+        //because language list is initialized now
+        google.charts.setOnLoadCallback(drawChart);
+
+        var $languages_list = $('#languages_list');
+    	$.each(language_list, function(index, val) {
     		 var text = val.name + ' - ' + val.amount;
     		 console.log(text);
-    		 $languages_list.append('<li><span class="list_text">' + text + '</span><div class="bar" style="width:' +  val.percentage_amount + '%"></div></li>');
+    		 $languages_list.append('<li><span class="list_text">' + text + '</span><div class="bar" style="width:' +  val.percentage_of_max + '%"></div></li>');
     	});
     });
+
+
+
+
+      
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Language');
+        data.addColumn('number', 'Repo count');
+        data.addRows(language_list.map(function(elem) {
+                            return [elem.name, elem.amount];
+                    })
+        );
+
+        // Set chart options
+        var options = {'title':'Repos by language',
+                       'width':'100%',
+                       'height':500};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
 })(jQuery);
